dfc8f69b9501dbe34505029a9753afadcb423a2e
tonychen <tonychen@TWC280-TONYNB>
2024-08-07 17:32:29 +0800

    【EventHandler】 Implement generic RoutedEventHandler.

---
+++ MenthaAssembly.WPFComponent/Misc/Events/RoutedEventHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace System.Windows
+{
+    /// <summary>
+    /// Represents the method that will handle various routed events that do not have specific event data beyond the data that is common for all routed events.
+    /// </summary>
+    /// <typeparam name="T">The specify type of event data.</typeparam>
+    /// <param name="sender">The object where the event handler is attached.</param>
+    /// <param name="e">The event data.</param>
+    public delegate void RoutedEventHandler<T>(object sender, T e)
+        where T : RoutedEventArgs;
+
+}
