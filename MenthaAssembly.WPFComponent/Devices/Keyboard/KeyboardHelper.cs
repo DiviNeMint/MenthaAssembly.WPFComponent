a5f44656edf4fe8f0d6e5c331dbeef396cdd108d
tonychen <tonychen@TWC280-TONYNB>
2024-08-07 17:30:58 +0800

    【KeyboardHelper】 Implement Key's extension function.

---
+++ MenthaAssembly.WPFComponent/Devices/Keyboard/KeyboardHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace System.Windows.Input
+{
+    public static class KeyboardHelper
+    {
+        public static bool IsLetter(this Key key)
+            => key is >= Key.A and <= Key.Z;
+
+        public static bool IsNumber(this Key key)
+            => key is >= Key.D0 and <= Key.D9 or >= Key.NumPad0 and <= Key.NumPad9;
+
+        public static bool IsNumberOrDot(this Key key)
+            => IsNumber(key) || key == Key.Decimal;
+
+        public static bool IsLetterOrNumber(this Key key)
+            => IsLetter(key) || IsNumber(key);
+
+        public static bool IsOperator(this Key key)
+            => key is Key.Divide or Key.Multiply or Key.Subtract or Key.Add;
+
+    }
+}
