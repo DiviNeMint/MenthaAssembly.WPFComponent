77ad33794a369cc502ea110e1ae44b1d0076afce
Mentha <asertyuk8@gmail.com>
2024-08-10 11:10:06 +0800

    【PropertyEditorItemMenuProvider】 Modify Folder Dialog type from【FolderBrowserDialog】to【OpenFolderDialog】at Framework Target NET8.0 and above.

--- MenthaAssembly.WPFComponent/Views/Editors/PropertyEditor/PropertyEditorItemMenuProvider.cs
+++ MenthaAssembly.WPFComponent/Views/Editors/PropertyEditor/PropertyEditorItemMenuProvider.cs
@@ -51,15 +51,26 @@ namespace MenthaAssembly.Views
                     switch (Option?.ExploreType)
                     {
                         case ExploreType.File:
-                            OpenFileDialog FileDialog = new OpenFileDialog();
-                            if (FileDialog.ShowDialog() is true)
-                                Property.SetValue(Target, FileDialog.FileName);
-                            break;
+                            {
+                                OpenFileDialog FileDialog = new OpenFileDialog();
+                                if (FileDialog.ShowDialog() is true)
+                                    Property.SetValue(Target, FileDialog.FileName);
+
+                                break;
+                            }
                         case ExploreType.Folder:
-                            FolderBrowserDialog FolderDialog = new FolderBrowserDialog();
-                            if (FolderDialog.ShowDialog() is true)
-                                Property.SetValue(Target, FolderDialog.SelectedPath);
-                            break;
+                            {
+#if NET8_0_OR_GREATER
+                                OpenFolderDialog FolderDialog = new();
+                                if (FolderDialog.ShowDialog() is true)
+                                    Property.SetValue(Target, FolderDialog.FolderName);
+#else
+                                FolderBrowserDialog FolderDialog = new();
+                                if (FolderDialog.ShowDialog() is true)
+                                    Property.SetValue(Target, FolderDialog.SelectedPath);
+#endif
+                                break;
+                            }
                     }
                 }
             };
