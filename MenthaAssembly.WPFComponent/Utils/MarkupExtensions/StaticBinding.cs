ab71dc774399def231154900cce12715d611b42c
tonychen <tonychen@TWC280-TONYNB>
2024-08-19 10:58:55 +0800

    【MarkupExtensions】 Disable StaticBinding.

--- MenthaAssembly.WPFComponent/Utils/MarkupExtensions/StaticBinding.cs
+++ MenthaAssembly.WPFComponent/Utils/MarkupExtensions/StaticBinding.cs
@@ -1,44 +1,44 @@
-﻿using System;
-using System.Windows.Data;
-using System.Windows.Markup;
+﻿//using System;
+//using System.Windows.Data;
+//using System.Windows.Markup;
 
-namespace MenthaAssembly.MarkupExtensions
-{
-    public class StaticBinding : MarkupExtension
-    {
-        public IValueConverter Converter { get; set; }
-        public object ConverterParameter { get; set; }
+//namespace MenthaAssembly.MarkupExtensions
+//{
+//    public class StaticBinding : MarkupExtension
+//    {
+//        public IValueConverter Converter { get; set; }
+//        public object ConverterParameter { get; set; }
 
-        private readonly string Path;
-        public StaticBinding(string Path)
-        {
-            this.Path = Path;
-        }
+//        private readonly string Path;
+//        public StaticBinding(string Path)
+//        {
+//            this.Path = Path;
+//        }
 
-        //var a = Provider.GetService(typeof(IXamlTypeResolver)) as IXamlTypeResolver;
-        //var b = a.Resolve("Views:TreeView");
+//        //var a = Provider.GetService(typeof(IXamlTypeResolver)) as IXamlTypeResolver;
+//        //var b = a.Resolve("Views:TreeView");
 
-        public override object ProvideValue(IServiceProvider Provider)
-            => Create(Path, Converter, ConverterParameter).ProvideValue(Provider);
+//        public override object ProvideValue(IServiceProvider Provider)
+//            => Create(Path, Converter, ConverterParameter).ProvideValue(Provider);
 
-        public static Binding Create(string Path, IValueConverter Converter = null, object ConverterParameter = null)
-        {
-            string[] Paths = Path.Split('.');
-            if (Path.Length < 2)
-                return null;
+//        public static Binding Create(string Path, IValueConverter Converter = null, object ConverterParameter = null)
+//        {
+//            string[] Paths = Path.Split('.');
+//            if (Path.Length < 2)
+//                return null;
 
-            if (Paths.Length.Equals(2))
-                return new Binding($"({Path})")
-                {
-                    Converter = Converter,
-                    ConverterParameter = ConverterParameter
-                };
+//            if (Paths.Length.Equals(2))
+//                return new Binding($"({Path})")
+//                {
+//                    Converter = Converter,
+//                    ConverterParameter = ConverterParameter
+//                };
 
-            return new Binding($"({string.Join(".", Paths, 0, 2)}).{string.Join(".", Paths, 2, Paths.Length - 2)}")
-            {
-                Converter = Converter,
-                ConverterParameter = ConverterParameter
-            };
-        }
-    }
-}
+//            return new Binding($"({string.Join(".", Paths, 0, 2)}).{string.Join(".", Paths, 2, Paths.Length - 2)}")
+//            {
+//                Converter = Converter,
+//                ConverterParameter = ConverterParameter
+//            };
+//        }
+//    }
+//}
