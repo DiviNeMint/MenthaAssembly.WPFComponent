0308a00381c6149c42dcad03860a0c0c73b1bc3a
tonychen <tonychen@TWC280-TONYNB>
2025-01-14 18:43:00 +0800

    【MiscValidationRule】 Rebuilds【MiscValidationRule】and change it to event validation judgment.

---
+++ MenthaAssembly.WPFComponent/Views/Misc/Events/RuleVerifyEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+
+namespace MenthaAssembly.Views
+{
+    public class RuleVerifyEventArgs(object Source, string PropertyName, object Value, DependencyObject Target, DependencyProperty TargetProperty) : EventArgs
+    {
+        public object Source { get; } = Source;
+
+        public string PropertyName { get; } = PropertyName;
+
+        public object Value { get; } = Value;
+
+        public DependencyObject Target { get; } = Target;
+
+        public DependencyProperty TargetProperty { get; } = TargetProperty;
+
+        public bool IsValid { get; set; }
+
+        public string Message { get; set; }
+
+    }
+}
---
+++ MenthaAssembly.WPFComponent/Views/Misc/MiscValidationRule.cs
@@ -0,0 +1,51 @@
+﻿using MenthaAssembly.Views;
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace MenthaAssembly.MarkupExtensions
+{
+    public sealed class MiscValidationRule : ValidationRule
+    {
+        public event EventHandler<RuleVerifyEventArgs> Verify;
+
+        public override ValidationResult Validate(object Value, CultureInfo CultureInfo, BindingExpressionBase Owner)
+        {
+            if (Verify != null &&
+                TryGetBindingDescription(Owner, out object Source, out string PropertyName, out DependencyObject Target, out DependencyProperty TargetProperty))
+            {
+                RuleVerifyEventArgs e = new(Source, PropertyName, Value, Target, TargetProperty);
+                Verify.Invoke(this, e);
+
+                if (!e.IsValid)
+                    return new ValidationResult(false, e.Message);
+            }
+
+            return ValidationResult.ValidResult;
+        }
+
+        private static bool TryGetBindingDescription(BindingExpressionBase BindingBase, out object Source, out string PropertyName, out DependencyObject Target, out DependencyProperty TargetProperty)
+        {
+            if (BindingBase is not BindingExpression Binding)
+            {
+                Source = null;
+                PropertyName = null;
+                Target = null;
+                TargetProperty = null;
+                return false;
+            }
+
+            Source = Binding.ResolvedSource;
+            PropertyName = Binding.ResolvedSourcePropertyName;
+            Target = Binding.Target;
+            TargetProperty = Binding.TargetProperty;
+            return true;
+        }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+            => throw new NotImplementedException();
+
+    }
+}
